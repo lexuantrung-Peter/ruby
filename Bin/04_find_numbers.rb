a04860a1f28e17303dbe1844c55558728a342d5a
Vinh Nguyen (Bin) <huuvinh@live.com>
2023-01-13 17:38:48 +0700

    fix 04

--- Bin/04_find_numbers.rb
+++ Bin/04_find_numbers.rb
@@ -1,17 +1,9 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_numbers(nums)
-    numStrings = []
-    nums.each do |n| 
-        numStrings << n.to_s
-    end
-
+    num_strings = []
+    nums.map { |n|  num_strings << n.to_s }
     count = 0
-    numStrings.each do |s|
-        if s.length % 2 == 0 
-            count += 1
-        end
-    end
+    num_strings.map {  |s| count += 1 if s.length % 2 == 0 }       
     count
-    
 end
