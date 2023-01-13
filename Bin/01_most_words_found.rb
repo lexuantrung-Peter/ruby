e2599ce528ea81ea4aa492cb2ff0e9de5b90bf5d
Vinh Nguyen (Bin) <huuvinh@live.com>
2023-01-13 17:05:11 +0700

    Update 01_most_words_found.rb
    fix feedbacks

--- Bin/01_most_words_found.rb
+++ Bin/01_most_words_found.rb
@@ -3,12 +3,10 @@
 def most_words_found(sentences)
     max = 0
     sentences.each do |s|
-        # puts("ssss")
-        length = s.split(' ').length 
-        if max < length
-            max = length
-        end
+      length = s.split(' ').length 
+       if max < length
+          max = length
+       end
     end
-    max
-    
+    max  
 end
