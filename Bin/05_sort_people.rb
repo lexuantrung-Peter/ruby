d00afbd21ac334de5ea4aeaf7424c09a918c1093
Vinh Nguyen (Bin) <huuvinh@live.com>
2023-01-13 17:41:44 +0700

    fix 05

--- Bin/05_sort_people.rb
+++ Bin/05_sort_people.rb
@@ -6,8 +6,6 @@ def sort_people(names, heights)
     heights.each_with_index do |height, index| 
         dict[height] = names[index]
     end
-
-    sorted_names = dict.sort_by { |key, value| key  }.reverse.map { |key, value| value }
-    sorted_names
+    sorted_names = dict.sort_by { |key, value| -key  }.to_h.values
     
 end
