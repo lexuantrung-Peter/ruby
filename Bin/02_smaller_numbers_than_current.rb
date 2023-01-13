430c1ce21101474c43abd6346e24fa80d915f3cf
Vinh Nguyen (Bin) <huuvinh@live.com>
2023-01-13 17:21:03 +0700

    fix 02

--- Bin/02_smaller_numbers_than_current.rb
+++ Bin/02_smaller_numbers_than_current.rb
@@ -2,17 +2,13 @@
 # @return {Integer[]}
 
 def smaller_numbers_than_current(nums)
-    outNums = []
+    out_nums = []
     nums.each do |n|
         count = 0
         nums.each do |m|
-            if m < n 
-                count += 1
-            end
+            count += 1 if m < n
         end
-        outNums << count
-
+        out_nums << count
     end
-    outNums
-    
+    out_nums  
 end
