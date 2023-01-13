dd2414007d57170a1b2fb0066294a85537fa2b7a
Vinh Nguyen (Bin) <huuvinh@live.com>
2023-01-13 17:34:06 +0700

    fix 03

--- Bin/03_array_string_equal.rb
+++ Bin/03_array_string_equal.rb
@@ -2,11 +2,6 @@
 # @param {String[]} word2
 # @return {Boolean}
 def array_strings_are_equal(word1, word2)
-    # array1 = word1.split(' ')
-    # array2 = word2.split(' ')
-    # array1.each_with_index do |c, index|
-
-    # end
     array1 = []
     word1.each do |w| 
         array1.concat(w.to_s.split(''))
@@ -16,9 +11,5 @@ def array_strings_are_equal(word1, word2)
     word2.each do |w|
         array2.concat(w.to_s.split(''))
     end
-    puts("array1 = #{array1}")
-    puts("array2 = #{array2}")
-    return array1 == array2
-
-    
+    array1 == array2
 end
